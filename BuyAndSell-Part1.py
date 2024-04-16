3887a762fdd2630f7fe107289053a553eb71e8b7
gihanswift <165775719+gihanswift@users.noreply.github.com>
2024-04-16 23:46:39 +0530

    Completed Max Profit

---
+++ BuyAndSell-Part1.py
@@ -0,0 +1,22 @@
+import sys
+
+
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        minPrice = sys.maxsize
+        maxPrice = 0
+        for price in prices:
+            minPrice =min(minPrice, price)
+            profit = price - minPrice
+            maxPrice = max(maxPrice, profit)
+
+        print(maxPrice)
+        return maxPrice
+
+
+
+
+
+
+sol = Solution()
+sol.maxProfit([7,1,5,3,6,4])
